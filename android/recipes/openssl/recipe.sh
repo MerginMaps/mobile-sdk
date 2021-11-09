ec928228c17d5b6ca5f341f910675bc23d8e4418
PeterPetrik <zilolv@gmail.com>
2021-11-09 12:37:07 +0100

    openssl android

--- android/recipes/openssl/recipe.sh
+++ android/recipes/openssl/recipe.sh
@@ -38,7 +38,10 @@ function build_openssl() {
   fi
 
   push_arm
-
+  export CC=$TOOLCHAIN_FULL_PREFIX-clang
+  export CFLAGS=""
+  export ANDROID_NDK_HOME=$ANDROIDNDK
+  
   try $BUILD_openssl/Configure shared ${SSL_ARCH} -D__ANDROID_API__=$ANDROIDAPI --prefix=/
   ${MAKE} depend
   ${MAKE} DESTDIR=${STAGE_PATH} SHLIB_VERSION_NUMBER= SHLIB_EXT=_1_1.so build_libs
