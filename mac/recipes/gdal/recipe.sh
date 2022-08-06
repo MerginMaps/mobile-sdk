2a1c57494a8cdc5b4c4e51d1ac17e8424cf81835
PeterPetrik <zilolv@gmail.com>
2022-08-06 11:59:11 +0200

    supported formats

--- mac/recipes/gdal/recipe.sh
+++ mac/recipes/gdal/recipe.sh
@@ -92,11 +92,6 @@ function build_gdal() {
   try $MAKESMP
   try $MAKESMP install
   
-  # we want to produce list of gdal formats supported here
-  # so we can add it to release artefacts
-  $STAGE_PATH/bin/gdalinfo --formats | tee -a $STAGE_PATH/supported_formats.log
-  $STAGE_PATH/bin/ogrinfo --formats | tee -a $STAGE_PATH/supported_formats.log
-  
   pop_env
 }
 
@@ -106,4 +101,9 @@ function postbuild_gdal() {
         error "Library was not successfully build for ${ARCH}"
         exit 1;
     fi
+    
+    # we want to produce list of gdal formats supported here
+    # so we can add it to release artefacts
+    $STAGE_PATH/bin/gdalinfo --formats | tee -a $STAGE_PATH/supported_formats.log
+    $STAGE_PATH/bin/ogrinfo --formats | tee -a $STAGE_PATH/supported_formats.log
 }
