f6523c83b2a421e1ba5b011fffa34024fbe07336
Peter Petrik <zilolv@gmail.com>
2023-08-09 14:00:42 +0200

    fix update qt script

--- scripts/update_qt_version.bash
+++ scripts/update_qt_version.bash
@@ -2,11 +2,6 @@
 
 set -e
 
-# qt6_poly2tri 
-# all worflows/*.yml
-# README
-
-
 echo "update_qt_version.bash MAJOR.MINOR.BUILD"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -16,27 +11,50 @@ MAJOR=${VER_PARTS[0]}
 MINOR=${VER_PARTS[1]}
 BUILD=${VER_PARTS[2]}
 
-echo "using version $MAJOR.$MINOR.$BUILD"
+echo "using QT version $MAJOR.$MINOR.$BUILD"
 
-# CMakeLists.txt
-CMAKE_FILE=$DIR/../CMakeLists.txt
-echo "patching $CMAKE_FILE"
-sed -i.orig -E "s|MM_VERSION_MAJOR \"[0-9]+\"|MM_VERSION_MAJOR \"$MAJOR\"|g" $CMAKE_FILE
-sed -i.orig -E "s|MM_VERSION_MINOR \"[0-9]+\"|MM_VERSION_MINOR \"$MINOR\"|g" $CMAKE_FILE
-sed -i.orig -E "s|MM_VERSION_PATCH \"[0-9]+\"|MM_VERSION_PATCH \"$BUILD\"|g" $CMAKE_FILE
-rm -f $CMAKE_FILE.orig
+############
+# README
+README_FILE=$DIR/../README.md
+echo "patching $README_FILE"
+# e.g. /Qt/6.5.2/ unix style
+sed -i.orig -E "s|/Qt/[0-9]+.[0-9]+.[0-9]+/|/Qt/${VERSION}/|g" $README_FILE
+# e.g. \Qt\6.5.2\ win style
+sed -i.orig -E "s|\\\Qt\\\[0-9]+.[0-9]+.[0-9]+\\\|\\\Qt\\\\${VERSION}\\\|g" $README_FILE
+rm -f $README_FILE.orig
 
-# .zenodo.json
-ZENODO_FILE=$DIR/../.zenodo.json
-echo "patching $ZENODO_FILE"
-sed -i.orig -E "s|\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"|\"version\": \"$MAJOR\.$MINOR\.$BUILD\"|g" $ZENODO_FILE
-sed -i.orig -E "s|https://github.com/MerginMaps/input/tree/[0-9]+\.[0-9]+\.[0-9]|https://github.com/MerginMaps/input/tree/$MAJOR\.$MINOR\.$BUILD|g" $ZENODO_FILE
-rm -f $ZENODO_FILE.orig
+############
+# WORKFLOWS
+for FNAME in \
+    $DIR/../.github/workflows/android.yml \
+    $DIR/../.github/workflows/ios.yml \
+    $DIR/../.github/workflows/linux.yml \
+    $DIR/../.github/workflows/mac.yml \
+    $DIR/../.github/workflows/win.yml
+do
+    echo "patching $FNAME"
+    # e.g. QT_VERSION: '6.5.2'
+    sed -i.orig -E "s|QT_VERSION: \'[0-9]+.[0-9]+.[0-9]+\'|QT_VERSION: \'${VERSION}\'|g" $FNAME
+    rm -f $FNAME.orig
+done
 
-# CITATION.cff
-CITATION_FILE=$DIR/../CITATION.cff
-echo "patching $CITATION_FILE"
-sed -i.orig -E "s|cff-version: [0-9]+\.[0-9]+\.[0-9]+|cff-version: $MAJOR\.$MINOR\.$BUILD|g" $CITATION_FILE
-rm -f $CITATION_FILE.orig
+############
+# qt6_poly2tri
+VCPKG_FILE=$DIR/../vcpkg-overlay/ports/qt6-poly2tri/vcpkg.json
+echo "patching $VCPKG_FILE"
+# e.g. "version": "6.5.2",
+sed -i.orig -E "s|\"version\": \"[0-9]+.[0-9]+.[0-9]+\"|\"version\": \"${VERSION}\"|g" $VCPKG_FILE
+rm -f $VCPKG_FILE.orig
 
+PORTFILE_FILE=$DIR/../vcpkg-overlay/ports/qt6-poly2tri/portfile.cmake
+echo "patching $PORTFILE_FILE"
+# e.g. qt/6.5/,
+sed -i.orig -E "s|qt/[0-9]+.[0-9]+/|qt/${MAJOR}.${MINOR}/|g" $PORTFILE_FILE
+rm -f $PORTFILE_FILE.orig
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!! You need to update SHA512 HASH in $PORTFILE_FILE. Download zip file, find SHA512 and update manually"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+# DONE
 echo "patching done"
