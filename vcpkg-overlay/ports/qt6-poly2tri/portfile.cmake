8c985f408d702f3599aefdbf75e45ca854be84d0
PeterPetrik <zilolv@gmail.com>
2023-11-01 19:24:42 +0100

    qt660

--- vcpkg-overlay/ports/qt6-poly2tri/portfile.cmake
+++ vcpkg-overlay/ports/qt6-poly2tri/portfile.cmake
@@ -3,9 +3,9 @@ if(NOT VCPKG_TARGET_IS_IOS)
     message(FATAL_ERROR "qt poly2tri only needed on iOS")
 else()
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://download.qt.io/archive/qt/6.5/${VERSION}/submodules/qtpositioning-everywhere-src-${VERSION}.zip"
+        URLS "https://download.qt.io/archive/qt/6.6/${VERSION}/submodules/qtpositioning-everywhere-src-${VERSION}.zip"
         FILENAME "qtpositioning-${VERSION}.zip"
-        SHA512 881b13513b5db84c1c5324e0f42b0ab5d6a7e60aa5b6958ff379213c8567b1732144488cca21894a6e3980094147e15ba5db31c47027bdec19dc6fe9bab13ee0
+        SHA512 927926c2efbc2f0fa94a4a33174a88c2c121f4e7ffa1f67370a9b88f4d0e1f22932c9c416cacd10562713daba8f1530688b81697bedebae84d727e0657dc052d
     )
     
     vcpkg_extract_source_archive(
